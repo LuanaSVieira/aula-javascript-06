f4c2b6cd41870c2df6b9b8b4d3f09f2cb718f31f
Luana Souza <87820972+LuanaSVieira@users.noreply.github.com>
2021-10-25 04:51:24 -0300

    Add files via upload

---
+++ string1.js
@@ -0,0 +1,26 @@
+// 1) 
+
+var nome = "Luana";
+var idade = 19;
+
+var conversao = idade.toString();
+console.log(conversao);
+
+var concatenacao = nome + idade;
+
+console.log(concatenacao);
+
+console.log(concatenacao.length);
+
+// 2)
+
+var num1 = 17;
+var num2 = 28;
+
+var conversao1 = num1.toString();
+var conversao2 = JSON.stringify(num2);
+
+var num3 = num1 + num2;
+
+console.log(num3);
+
---
+++ string2.js
@@ -0,0 +1,80 @@
+// a)
+
+let str = "abcdefghijklm";
+let resultado = str.substring(8); 
+console.log(resultado);
+
+// c)
+
+let txt = "Olá, mundo";
+let resultado = txt.replace("Olá", "Bem vindo");
+
+console.log(resultado);
+
+// d) 
+
+let txt = "Olá mundo";
+let resultado = txt.toLowerCase();
+
+console.log(resultado);
+
+// e)
+
+let txt = "Olá mundo";
+let resultado = txt.toUpperCase();
+
+console.log(resultado);
+
+// f)
+
+let txt = "Posso comer bananas o dia todo";
+let tamanho = txt.length;
+
+console.log(tamanho);
+
+// g)
+
+let str1 = "Hello";
+let str2 = "World";
+let str3 = str1 + str2;
+
+
+console.log(str3)
+
+// h)
+
+const fruits = ["banana", "Orange", "Apple", "Kiwi"];
+fruits.sort();
+
+console.log(fruits);
+
+// i)
+
+const fruits = ["banana", "Orange", "Apple", "Kiwi"];
+fruits.shift();
+fruits.pop();
+
+console.log(fruits);
+
+// j)
+
+const fruits = ["banana", "Orange", "Apple"];
+fruits.push("uva", "caju", "kiwi");
+
+console.log(fruits);
+
+// k)
+
+const fruits = ["banana", "Orange", "Apple"];
+fruits.unshift("Pera", "manga");
+fruits.pop;
+
+console.log(fruits);
+
+
+
+
+
+
+
+
